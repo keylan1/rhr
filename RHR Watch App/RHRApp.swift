640c46202b4f531e774fce3cf8dbff843928dec4
keylan1 <sarah@frybad.de>
2024-10-14 15:24:34 +0200

    Initial Commit

---
+++ RHR Watch App/RHRApp.swift
@@ -0,0 +1,17 @@
+//
+//  RHRApp.swift
+//  RHR Watch App
+//
+//  Created by Sarah Keller on 14.10.24.
+//
+
+import SwiftUI
+
+@main
+struct RHR_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RHR Watch AppTests/RHR_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RHR_Watch_AppTests.swift
+//  RHR Watch AppTests
+//
+//  Created by Sarah Keller on 14.10.24.
+//
+
+import Testing
+@testable import RHR_Watch_App
+
+struct RHR_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
