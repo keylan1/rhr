8dfe498813a42b9314b1c5d743cbe810aec2e562
keylan1 <sarah@frybad.de>
2024-11-13 15:13:34 +0100

    Added file

---
+++ RHR Watch App/NotificationManager.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationManager.swift
+//  RHR Watch App
+//
+//  Created by Sarah Keller on 13.11.24.
+//
+
+import Foundation
+
+
+//Request auth
+
+//Check notification status
+
+//Notification format and trigger
+
+//Trigger based on status
